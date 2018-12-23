e23f8a31926438bf631cf8c38dfe7681c93aa51b
Ahmadou Dicko <mail@ahmadoudicko.com>
2018-12-23 13:23:47 +0000

    Push new pkg name

--- R/hi.R
+++ R/hi.R
@@ -48,7 +48,7 @@ rm(hi_constructor)
 hi <- function(name = "Livestock", size = 1, fixed_width = FALSE, animate = "still",
                rotate = 0, flip = "none", border = FALSE, pull = NULL, color = NULL, colour = color, other = NULL) {
     if(!(name %in% hi_iconList)) {
-        stop(paste0("Icon '", name, "' not found in Humanitarian icons. Did you mean '", hi_iconList[which.min(adist(name, hi_iconList))], "'?"))
+        stop(paste0("Icon '", name, "' not found in OCHA Humanitarian Icons v02. Did you mean '", hi_iconList[which.min(adist(name, hi_iconList))], "'?"))
     }
     result <- structure(list(name = name, options = list(size = size, fixed_width = fixed_width,
                                                          animate = animate, rotate = rotate, flip = flip,

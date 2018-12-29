1bcc2ba76392291957f0d587b7ccc6b7929685ad
Ahmadou Dicko <mail@ahmadoudicko.com>
2018-12-29 13:50:40 +0000

    Add function to get unicode

--- R/aaa.R
+++ R/aaa.R
@@ -10,3 +10,21 @@ get_humiconList <- function(path) {
 icon_system_file <- function(file) {
   system.file(file, package = "humicons")
 }
+
+#' Get OCHA Humanitarian icons v02 unicode
+#' 
+#' Get you the unicode to use in base graphic or ggplot2
+#' @param name character name of the icon see humicons_data
+#' @examples
+#' get_humicon("Urban")
+#' 
+#' @export
+#'
+get_humicon <- function(name) {
+    df <- humicons_data
+    icon_list <- df$name
+    if(!(name %in% icon_list)) {
+        stop(paste0("Icon '", name, "' not found in OCHA Humaniarian Icons v02. Did you mean '", icon_list[which.min(adist(name, icon_list))], "'?"))
+    }
+    intToUtf8(strtoi(df$code[df$name == name], 16L))
+}

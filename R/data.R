3975febe5f586d2312bbb055119b55c6a3c921df
Ahmadou Dicko <mail@ahmadoudicko.com>
2018-12-29 13:15:15 +0000

    Add icon data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' OCHA Humanitarian fonts v02 data in tibble format
+#'
+#' A dataset containing the name and code for all 295 humanitarian icons 
+#'
+#' @format A tibble with 295 rows and 2 variables:
+#' \describe{
+#'   \item{name}{name of the icon}
+#'   \item{code}{code of the icon}
+#' }
+#' @source \url{https://thenounproject.com/ochavisual/collection/ocha-humanitarian-icons-v02/}
+"humicons_data"

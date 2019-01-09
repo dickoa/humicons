de2de72a1cd036da4f81c33b2914749e99f4bb6a
Ahmadou Dicko <mail@ahmadoudicko.com>
2019-01-09 12:33:39 +0000

    Vectorized version of humicon(s)

--- R/aaa.R
+++ R/aaa.R
@@ -6,25 +6,30 @@ get_humiconList <- function(path) {
     r[!grepl("\\{", r)]
 }
 
-
 icon_system_file <- function(file) {
   system.file(file, package = "humicons")
 }
 
+humicon <- function(name) {
+    df <- humicons_data
+    icon_list <- df$name
+    bool <- tolower(name) %in% tolower(icon_list)
+    if(!bool) {
+        stop(paste0("Icon '", name, "' not found in OCHA Humaniarian Icons v02. Did you mean '", icon_list[which.min(adist(name, icon_list))], "'?"))
+    }
+    code <- df$code[tolower(df$name) == tolower(name)]
+    intToUtf8(strtoi(code, 16L))
+}
+
+
 #' Get OCHA Humanitarian icons v02 unicode
 #' 
 #' Get you the unicode to use in base graphic or ggplot2
-#' @param name character name of the icon see humicons_data
+#' @param names character name of the icon see humicons_data
 #' @examples
-#' get_humicon("Urban")
+#' humicons(c("Urban", "Car"))
 #' 
 #' @export
-#'
-get_humicon <- function(name) {
-    df <- humicons_data
-    icon_list <- df$name
-    if(!(name %in% icon_list)) {
-        stop(paste0("Icon '", name, "' not found in OCHA Humaniarian Icons v02. Did you mean '", icon_list[which.min(adist(name, icon_list))], "'?"))
-    }
-    intToUtf8(strtoi(df$code[df$name == name], 16L))
+humicons <- function(names) {
+    vapply(names, humicon, character(1))
 }
